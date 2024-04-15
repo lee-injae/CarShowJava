ccda50f68e2b04f2bb22837de304ec89d6e96036
Injae Lee <ilee.injae@gmail.com>
2024-04-15 16:09:36 -0400

    completed car repository

--- src/main/java/com/blit/repositories/CarRepository.java
+++ src/main/java/com/blit/repositories/CarRepository.java
@@ -1,9 +1,23 @@
 package com.blit.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blit.models.Car;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-
+	
+	//fetch car by brand
+	List<Car> findCarByBrand(String brand);
+	//fetch car by color
+	List<Car> findCarByColor(String color); 
+	//fetch car by year
+	List<Car> findCarByYear(int year);
+	//fetch car by color and model
+	List<Car> findCarByBrandAndModel(String brand, String model);
+	//fetch car by year and sort by year
+	List<Car> findCarByBrandOrderByYearAsc(String brand);
+	
+	
 }
